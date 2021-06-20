b4cb07c4ebc899c6ec26ca540a2bd4d59e712ea2
carlos <crmm17.cm@gmail.com>
2021-06-20 18:49:40 -0500

    Create address seeds to populate database
    Create states, municipalities and zip codes seeds to populate database with actual info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'csv'
+
+puts 'Importing states...'
+CSV.foreach(Rails.root.join('db/seeds/states.csv'), headers: true) do |row|
+  State.create! do |state|
+    state.id = row[0]
+    state.name = row[1]
+  end
+end
+
+puts 'Importing municipalities...'
+CSV.foreach(Rails.root.join('db/seeds/municipalities.csv'), headers: true) do |row|
+  Municipality.create! do |municipality|
+    municipality.state_id = row[0]
+    municipality.name = row[1]
+  end
+end
+
+puts 'Importing zip codes...'
+CSV.foreach(Rails.root.join('db/seeds/zip_codes.csv'), headers: true) do |row|
+  ZipCode.create! do |zip_code|
+    zip_code.municipality_id = row[0]
+    zip_code.name = row[1]
+  end
+end
